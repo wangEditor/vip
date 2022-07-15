72489e69cf27abaa320f3b2bc8cf71b48b3f0281
wangEditor <wangeditor@126.com>
2022-07-15 17:15:18 +0800

    Update register-site.js

--- register-site.js
+++ register-site.js
@@ -5,7 +5,7 @@
 
 try {
   window.__wangEditorRegisterVipSite([
-
+    { hostname: 'www.a.com', expire: '2023-8-15 23:59:59', desc: '测试' },
     {
       hostname: 'www.wangeditor.com', // location.hostname
       expire: '2023-8-15 23:59:59', // 失效时间
@@ -13,6 +13,8 @@ try {
     },
 
     // 其他网站
+    
+    { hostname: 'www.b.com', expire: '2023-8-15 23:59:59', desc: '测试' }
   ])
 } catch(err) {
   console.warn('wangEditorRegisterVipSite error', err)
